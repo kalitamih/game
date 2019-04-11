2f01cf4b041d30d9bc2263157156bc5462a490dc
Kalita Mikhail <mkalita1991@gmail.com>
2019-04-11 16:53:14 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/game/precache-manifest.124fbd4d4ad8f4875d0955dee20d7234.js"
+  "/game/precache-manifest.dd59571ba622a0d5134f999eac46cf31.js"
 );
 
 workbox.clientsClaim();
